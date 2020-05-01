213500de2efa18263df6948950ab405a14e96d91
lmshasta <lindanismoyo@gmail.com>
2020-05-01 16:45:22 +0200

    machine learning

---
+++ data_sc_scratch.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+
+topics to study
+#chapter 5, statistics, c
+#chapter 6, probability - dependence and independence
+#chapter 6,  probability - conditional probability
+#chapter 6,  probability - Bayes’s Theorem
+#chapter 6,  probability - Continuous Distributions
+#chapter 6,  probability - The Normal Distribution
+#chapter 6,  probability - The Central Limit Theorem
+#Chapter 7. Hypothesis and Inference
+# Statistical Hypothesis Testing
+# Confidence Intervals
+# P-hacking
+# Bayesian Inference
+
+# Chapter 11. Machine Learning
+# Before we can talk about machine learning we need to talk about models.
+# What is a model? It’s simply a specification of a mathematical (or probabilistic)
+# relationship that exists between different variables.
+
+
